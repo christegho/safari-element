a8a20fd6e195acbd12956af03bf72489ab3a229e
Chris Tegho <christegho@gmail.com>
2020-11-25 13:22:20 +0100

    Refactoring files for converting supervisely format to VOC format

--- rename_files.py
+++ rename_files.py
@@ -1,36 +1,38 @@
 import glob, shutil
+import config as cfg
 
 def remove_last_occurence(mystr, removal, replacement):
     reverse_removal = removal[::-1]
     reverse_replacement = replacement[::-1]
     return mystr[::-1].replace(reverse_removal, reverse_replacement, 1)[::-1]
 
-patterns = ['not_can_ann/*/*.png', 'not_can_ann/*/*.jpg', 'not_can_ann/*/*.jpeg']
-# patterns = ['cansynth/2019*/*/*/*.png', 'cansynth/2019*/*/*/*.jpg', 'cansynth/2019*/*/*/*.jpeg']
 
-for pattern in patterns:
-    for file in glob.glob(pattern):
-        print(file)
-        new_path = file.replace('.', '_', file.count('.')-1)
-        new_path = new_path.replace('_jpg', '')
-        new_path = new_path.replace('_jpeg', '')
-        new_path = new_path.replace('_png', '')
-        new_path = new_path.replace('.jpeg', '.png')
-        new_path = new_path.replace('.jpg', '.png')
-        shutil.move(file, new_path)
+def rename_files():
+    for pattern in cfg.img_patterns:
+        for file_path in glob.glob(pattern):
+            print(file_path)
+            new_path = file_path.replace('.', '_', file_path.count('.')-1)
+            new_path = new_path.replace('_jpg', '')
+            new_path = new_path.replace('_jpeg', '')
+            new_path = new_path.replace('_png', '')
+            new_path = new_path.replace('.jpeg', '.png')
+            new_path = new_path.replace('.jpg', '.png')
+            new_path = new_path.replace(' ', '_')
+            shutil.move(file_path, new_path)
+
+
+    for pattern in cfg.json_path_pattern:
+        for file_path in glob.glob(pattern):
+            print(file_path)
+            new_path = remove_last_occurence(file_path, '.jpeg', '')
+            new_path = remove_last_occurence(new_path, '.jpg', '')
+            new_path = remove_last_occurence(new_path, '.png', '')
+            new_path = file_path.replace('.', '_', file_path.count('.')-1)
+            new_path = new_path.replace('_jpg', '')
+            new_path = new_path.replace('_jpeg', '')
+            new_path = new_path.replace('_png', '')
+            new_path = new_path.replace(' ', '_')
+            shutil.move(file_path, new_path)
         
-      
-patterns = ['not_can_ann/*/*.json'] #['*/*canisters*/*/*/*.json']
-# patterns = ['cansynth/2019*/*/*/*.json']
-
-for pattern in patterns:
-    for file in glob.glob(pattern):
-        print(file)
-        new_path = remove_last_occurence(file, '.jpeg', '')
-        new_path = remove_last_occurence(new_path, '.jpg', '')
-        new_path = remove_last_occurence(new_path, '.png', '')
-        new_path = file.replace('.', '_', file.count('.')-1)
-        new_path = new_path.replace('_jpg', '')
-        new_path = new_path.replace('_jpeg', '')
-        new_path = new_path.replace('_png', '')
-        shutil.move(file, new_path)
+if __name__ == "__main__":
+    rename_files()
--- supervisely_to_pascal_voc.py
+++ supervisely_to_pascal_voc.py
@@ -1,35 +1,6 @@
 import glob, json, os, shutil
 from pascal_voc_writer import *
-
-voc_folder_name = 'CanistersSynthetic'
-dataset = 'synth'
-json_path_pattern = 'cansynth/2019*/*/*/*.json' #'not_can/*/*.json'
-
-"""
-Hierarchy:
-Data Directory
--> images
---> image0.png
---> image1.png
---> imageN.png
--> labels
---> labels0.txt
---> labels1.txt
---> labelsN.txt
-
-In other words labels folder should be located next to the image folder in the same directory named "labels".
-"""
-
-if not os.path.exists(voc_folder_name):
-    os.mkdir(voc_folder_name)
-
-for subfolder in ['Annotations', 'JPEGImages', 'labels', 'ImageSets', 'ImageSets/Main']:
-    if not os.path.exists(os.path.join(voc_folder_name, subfolder)):
-        os.mkdir(os.path.join(voc_folder_name, subfolder))
-
-image_index = 0
-image_set = open(os.path.join(voc_folder_name, 'ImageSets', 'Main', 'triplecanister_{}'.format(dataset)), 'w')
-list_file = open('{}.txt'.format(dataset), 'w')
+import config as cfg
 
 def convert(size, box):
     dw = 1./size[0]
@@ -44,44 +15,75 @@ def convert(size, box):
     h = h*dh
     return (x,y,w,h)
 
-for file in glob.glob(json_path_pattern):
-    print(file)
-    with open(file) as json_file:
-        data = json.load(json_file)
-        image_path = file.split('/')
-        image_path[-1] = image_path[-1].split('.')[0] + '.png'
-        image_path[-2] = 'img'
-        image_path = os.path.join(*image_path)
-        image_name = str(image_index).zfill(5)
-        image_set.write(image_name+'\n')
-        new_image_path = '{}/JPEGImages/{}.jpg'.format(voc_folder_name, image_name)
-        list_file.write(new_image_path+'\n')
-        image_index += 1
-        try:
-            shutil.copy(image_path, new_image_path)
-        except:
-            print('error', image_path, new_image_path)
-        
-        label_txt = open('{}/labels/{}.txt'.format(voc_folder_name, image_name), 'w')
-        w = data["size"]["width"]
-        h = data["size"]["height"]
-        writer = Writer(new_image_path, w, h)
-        for detBox in data['objects']:
-            if detBox['bitmap'] == None and detBox['classTitle'] == 'canister-general-bbox':
-                p1, p2 = detBox['points']['exterior']
-                x1, y1 = p1
-                x2, y2 = p2
-                x_min = min(x1, x2)
-                x_max = max(x1, x2)
-                y_min = min(y1, y2)
-                y_max = max(y1, y2)
-                writer.addObject('canister', x_min, y_min, x_max, y_max)
-                bb = convert((int(w), int(h)), [float(a) for a in [x_min, x_max, y_min, y_max]])
-                label_txt.write(str(0) + " " + " ".join([str(a) for a in bb]) + '\n')
-        
-        writer.save('{}/Annotations/{}.xml'.format(voc_folder_name, image_name))
-        label_txt.close()
-        
-list_file.close()        
-image_set.close()
+def supervisely_to_pascal_voc():
+    """
+    Hierarchy:
+    Data Directory
+    -> images
+    --> image0.png
+    --> image1.png
+    --> imageN.png
+    -> labels
+    --> labels0.txt
+    --> labels1.txt
+    --> labelsN.txt
+
+    In other words labels folder should be located next to the image folder in the same directory named "labels".
+    """    
+    if not os.path.exists(cfg.voc_folder_name):
+        os.mkdir(cfg.voc_folder_name)
+
+    for subfolder in ['Annotations', 'JPEGImages', 'labels', 'ImageSets', 'ImageSets/Main']:
+        if not os.path.exists(os.path.join(cfg.voc_folder_name, subfolder)):
+            os.mkdir(os.path.join(cfg.voc_folder_name, subfolder))
+
+    image_index = 0
+    image_set = open(os.path.join(cfg.voc_folder_name, 'ImageSets', 'Main', cfg.dataset, 'w')
+    list_file = open('{}.txt'.format(cfg.dataset), 'w')
+    for json_path_pattern in cfg.json_path_pattern:
+        for file in glob.glob(json_path_pattern):
+            print(file)
+            with open(file) as json_file:
+                data = json.load(json_file)
+                image_path = file.split('/')
+                image_path[-1] = image_path[-1].split('.')[0] + '.png'
+                image_path[-2] = 'img'
+                image_path = os.path.join(*image_path)
+                image_name = cfg.prefix_im_name + '_' + str(image_index).zfill(5)
+                image_set.write(image_name+'\n')
+                new_image_path = '{}/JPEGImages/{}.jpg'.format(cfg.voc_folder_name, image_name)
+                list_file.write(new_image_path+'\n')
+                image_index += 1
+                try:
+                    shutil.copy(image_path, new_image_path)
+                except:
+                    print('error', image_path, new_image_path)
+
+                label_txt = open('{}/labels/{}.txt'.format(cfg.voc_folder_name, image_name), 'w')
+                w = data["size"]["width"]
+                h = data["size"]["height"]
+                writer = Writer(new_image_path, w, h)
+                for detBox in data['objects']:
+                    if ('bitmap' not in detBox or ('bitmap' in detBox and not detBox['bitmap'])) \
+                        and detBox['classTitle'] in cfg.class_mapping:
+                        classname = detBox['classTitle']
+                        p1, p2 = detBox['points']['exterior']
+                        x1, y1 = p1
+                        x2, y2 = p2
+                        x_min = min(x1, x2)
+                        x_max = max(x1, x2)
+                        y_min = min(y1, y2)
+                        y_max = max(y1, y2)
+                        writer.addObject(cfg.class_mapping[classname], x_min, y_min, x_max, y_max)
+                        bb = convert((int(w), int(h)), [float(a) for a in [x_min, x_max, y_min, y_max]])
+                        label_txt.write(str(cfg.classes_conversion[classname]) + " " + " ".join([str(a) for a in bb]) + '\n')
+
+                writer.save('{}/Annotations/{}.xml'.format(cfg.voc_folder_name, image_name))
+                label_txt.close()
+
+    list_file.close()        
+    image_set.close()
+
+if __name__ == "__main__":
+    supervisely_to_pascal_voc()
                 
